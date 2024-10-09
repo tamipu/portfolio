9a200a2bf3b760efeac785511c519d39fc6e458e
Phuong TA <104944175+tamipu@users.noreply.github.com>
2024-10-09 15:08:17 +0200

    separated css

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,34 +1,19 @@
 import React from 'react';
+import './Hero.css';
 
 const Hero: React.FC = () => {
     return (
-        <section
-            className="hero d-flex flex-column justify-content-center align-items-center text-center p-5"
-            style={{ height: '75vh' }}>
-            <div className="avatar-container mb-4" style={{ position: 'relative', padding: '5px' }}>
-                <div
-                    className="ring"
-                    style={{
-                        width: '180px',
-                        height: '180px',
-                        borderRadius: '50%',
-                        display: 'flex',
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                        background: 'linear-gradient(135deg, #f09, #3023ae, #00f, #ff0)',
-                        backgroundSize: '400% 400%',
-                        animation: 'changeColor 8s ease infinite',
-                    }}
-                >
+        <section className="hero">
+            <div className="avatar-container">
+                <div className="ring">
                     <img
                         src="/images/photo.png"
                         alt="Phuong's Avatar"
-                        className="avatar rounded-circle"
-                        style={{ width: '170px', height: '170px', objectFit: 'cover' }}
+                        className="avatar"
                     />
                 </div>
             </div>
-            <h2 className="display-4" style={{ fontFamily: 'Lobster, cursive', fontSize: '4rem' }}>
+            <h2 className="display-4">
                 Mia <span style={{ color: 'rgb(112,169,211)' }}>Phuong</span>
             </h2>
             <p className="lead">Full Stack Developer</p>
