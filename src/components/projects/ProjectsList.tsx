c5289608afcd8e3decf442cce388d75ed97828e9
Phuong TA <104944175+tamipu@users.noreply.github.com>
2025-02-23 17:39:46 +0100

    modified project

--- src/components/projects/ProjectsList.tsx
+++ src/components/projects/ProjectsList.tsx
@@ -46,12 +46,6 @@ Applied the MERISE methodology to ensure a structured and coherent database desi
                     techStack={[]}
                 />
                 <ProjectCard
-                    title="Charts Application"
-                    description="A Spring Boot application that provides a REST API for generating chart templates and series data using Apache ECharts."
-                    githubLink="https://github.com/tamipu/echarts-springboot-api"
-                    techStack={['Java']}
-                />
-                <ProjectCard
                     title="Mood Tracker with Visual Journal"
                     description="Developed a Mood Tracker web application using TypeScript, React with an interactive visual journal, enabling users to record and monitor their mood over time.
 Integrated charting libraries to visualize users' mood patterns in graphical formats, allowing for better insights into their emotional well-being."
